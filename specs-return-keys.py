fceb2115fc3e464a6d3f31a655c68dd7c8d980b1
margaridamarina <margaridammcm@gmail.com>
2023-10-04 15:27:12 -0300

    remove sublists

--- specs-return-keys.py
+++ specs-return-keys.py
@@ -26,11 +26,23 @@ def get_list_of_keys(csv_filename):
                 list_of_keys.extend(keys)
     return list_of_keys
 
+def is_subset(sublist, superlist):
+    return all(item in superlist for item in sublist)
+
+def remove_sublists(list_of_lists):
+    result = []
+    for sublist in list_of_lists:
+        if not any(is_subset(sublist, other) for other in list_of_lists if sublist != other):
+            result.append(sublist)
+    return result
+
 if __name__ == '__main__':
     data = get_list_of_keys("query_result.csv")
     sorted_list_keys = [sorted(x) for x in data]
     joined_list_keys = ['+'.join(x) for x in sorted_list_keys]
     unique_list_of_keys = [x.split('+') for x in list(set(joined_list_keys))]
-    for unk_list_key in unique_list_of_keys:
-        print(unk_list_key)
     
+    filtered_list = remove_sublists(unique_list_of_keys)
+    
+    for unk_list_key in filtered_list:
+        print(unk_list_key)
