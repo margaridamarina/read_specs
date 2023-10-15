ebeb4a877543d241bc3888b02ea6d5c1b4e29cf2
margaridamarina <margaridammcm@gmail.com>
2023-10-15 09:50:47 -0300

    keys return specs

--- keys-return-specs.py
+++ keys-return-specs.py
@@ -3,7 +3,7 @@ import json
 
 def get_keys(spec):
     list_keys = []
-    unwanted_keys = ['type', 'name', 'json_path', 'sample', 'required', 'condition', 'avoid_retry', 'emit_event']
+    unwanted_keys = ['type', 'name', 'sample', 'required', 'condition', 'avoid_retry', 'emit_event']
     for step in spec.get("steps", []):
         if step.get("type") == "parser":
             for field in step.get("fields", []):
@@ -22,35 +22,36 @@ def get_list_of_keys(csv_filename):
         csvreader = csv.reader(csvfile)
         next(csvreader)  
 
-        for row_number, row in enumerate(csvreader, start=2):  # Começa em 2 para contar a partir da linha 2
+        for row_number, row in enumerate(csvreader, start=2):  
             if row[4]:
                 spec_str = row[4]
                 spec_dict = json.loads(spec_str)
                 keys = get_keys(spec_dict)
-                list_of_keys.append(keys)
+                list_of_keys.append((row[0], row[2], keys))  # Adiciona também o número do crawler e o projeto
 
     return list_of_keys
 
 if __name__ == '__main__':
     data = get_list_of_keys("query_result.csv")
     
-    key_to_row_mapping = {}  # Dicionário para mapear listas de chaves para linhas do CSV
+    key_to_row_mapping = {}  
     with open("query_result.csv", newline='') as csvfile:
         csvreader = csv.reader(csvfile)
         next(csvreader)  
 
-        for row_number, row in enumerate(csvreader, start=2):  # Começa em 2 para contar a partir da linha 2
+        for row_number, row in enumerate(csvreader, start=2):  
             if row[4]:
                 spec_str = row[4]
                 spec_dict = json.loads(spec_str)
                 keys = get_keys(spec_dict)
                 key_string = list_of_keys_to_string(keys)
-                key_to_row_mapping[key_string] = row_number  # Mapeia a lista de chaves para o número da linha
+                key_to_row_mapping[key_string] = (row[0], row[2], row_number)  # Mapeia a lista de chaves para o número da linha, o número do crawler e o nome do projeto
     
-    unique_list_of_keys = list(set(list_of_keys_to_string(keys) for keys in data))
+    unique_list_of_keys = list(set(list_of_keys_to_string(keys) for _, _, keys in data))
     for unique_key in unique_list_of_keys:
         print(f"Lista de Chaves: {unique_key}")
-        row_number = key_to_row_mapping.get(unique_key)
+        crawler_number, project_name, row_number = key_to_row_mapping.get(unique_key, (None, None, None))
         if row_number:
-            print(f"Linha do CSV: {row_number} " )
-        print()
+            print(f"Numero do Crawler: {crawler_number}")
+            print(f"Projeto: {project_name}")
+            print(f"Linha do CSV: {row_number}" + "\n")
